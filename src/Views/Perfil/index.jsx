9481071fedd3d163f9da7827d5b7b24c7ed16511
Estela Souza <estelasouza@192.168.1.12>
2022-04-10 15:40:33 -0300

    add navbar

---
+++ src/Views/Perfil/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css' 
+
+export function Perfil( ){
+    return (
+        <>
+        <h1 className='name color'> testando </h1>
+        <h1 className='name color'> testando </h1>
+        <h1 className='name color'> testando </h1>
+        </>
+    )
+}
