6f0a80cef0004b0995fde48500930b45e06dd267
Sidney Alex <silvercenturion2@gmail.com>
2022-04-13 00:08:34 -0300

    add capacitor config

--- src/Views/Login/Login.jsx
+++ src/Views/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../../Assets/Images/logo.svg';
+import logo from '../../assets/images/logo.svg';
 import '../IsLogged/IsLogged.css'
 
 const Login = (props) => {
--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,33 @@
-import ReactDOM  from 'react-dom';
+import ReactDOM from 'react-dom';
 import { Switch, BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import { useState } from 'react' 
-import GlobalStyle from './Global/globalStyle';
+import { useEffect, useState } from 'react'
+import GlobalStyle from './global/globalStyle';
 import { Perfil } from './Views/Perfil';
 
 import IsLogged from './Views/IsLogged/IsLogged';
-import  NavBar  from './Components/NavBar';
+import NavBar from './Components/NavBar';
+
+//Capacitor
+import { StatusBar, Style } from '@capacitor/status-bar';
+StatusBar.setOverlaysWebView({ overlay: false });
+StatusBar.setStyle({ style: Style.Dark });
+StatusBar.setBackgroundColor({color:'#1976d2'})
 
 function PlataformRotas() {
- 
+
   return (
     <Switch>
-      <Route path="/initial" component={Perfil}  />
+      <Route path="/initial" component={Perfil} />
       <Route path="/profile" component={Perfil} />
 
     </Switch>
-    
+
   );
 }
 
 function App() {
   // const [uuid, setUuid] = useState(localStorage.getItem("uid"));
-  let isAuth ='key';
+  let isAuth = 'key';
 
   // const handleLogin = (uuid) => {
   //   setUuid(uuid);
@@ -30,26 +36,26 @@ function App() {
   //   setUuid(null);
   // };
 
-  return (
-      <>
-        <Router>
-          <Switch>
-            { isAuth ? (
-              <>
+return (
+    <>
+      <Router>
+        <Switch>
+          {isAuth ? (
+            <>
               <NavBar />
               <PlataformRotas />
-              </>
-            ): (
-              <>
+            </>
+          ) : (
+            <>
               <Redirect to='/' />
               <Route component={IsLogged} exact path="/" />
-              
-              </>
-            )}
-          </Switch>
-        </Router>
-        <GlobalStyle />
-      </>
+
+            </>
+          )}
+        </Switch>
+      </Router>
+      <GlobalStyle />
+    </>
   );
 }
 
