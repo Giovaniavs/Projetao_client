b931fd78d0c5965b47216456d3e538f870dd71b8
Estela Souza <estelasouza@Estelas-MacBook-Air.local>
2022-04-17 19:59:18 -0300

    new component

---
+++ src/components/CreateGroup/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function CreateNewGroup(){
+    return(
+        <>
+        <p>nome</p>
+        <input />
+        <p>nome</p>
+        <input /> <p>nome</p>
+        <input />
+        </>
+    )
+}
