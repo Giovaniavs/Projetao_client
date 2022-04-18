98bc828856a71238d83e1b76d4069c4c32bde3ef
Estela Souza <estelasouza@Estelas-MacBook-Air.local>
2022-04-17 21:49:29 -0300

    create inputs

--- src/components/CreateGroup/index.jsx
+++ src/components/CreateGroup/index.jsx
@@ -1,13 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { InputType, TextType,Button, CenterDiv  } from "./styles";
+
 
 export function CreateNewGroup(){
+    const [input,setInput] = useState({
+        group:"",
+        address:"",
+        email:""
+    })
+    const [grupo,setGrupo] = useState([])
+    const handleChange = (e) =>{
+        const { name, value } = e.target;
+        setInput(prevState => ({
+            ...prevState,
+            [name]: value}))
+            
+    }
+    const handleGroup = (e) => {
+        e.preventDefault()
+        setGrupo(old=>[...old,input])
+        setInput({
+            group:"",
+            address:"",
+            email:""
+        })
+    console.log(grupo)
+    }
+   
     return(
         <>
-        <p>nome</p>
-        <input />
-        <p>nome</p>
-        <input /> <p>nome</p>
-        <input />
+           <CenterDiv>
+      
+         <TextType>Nome do grupo </TextType>
+        <InputType   
+        type="text"
+          value={input.group}
+          onChange={handleChange}
+          name="group"
+          />
+        
+        <TextType>Endereco </TextType>
+        <InputType   
+        type="text"
+          value={input.address}
+          onChange={handleChange}
+          name="address"
+           />
+        <TextType>Email </TextType>
+        <InputType   type="text"
+          value={input.email}
+          onChange={handleChange}
+          name="email"
+           />
+           <Button  onClick={handleGroup} >Enviar</Button>
+        
+       </CenterDiv>
         </>
     )
 }
---
+++ src/components/CreateGroup/styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const InputType = styled.input`
+    width:300px;
+    height: 30px;
+    margin:10px;
+    border: 1px solid blue;
+    border-radius:5px;
+    
+`
+
+export const CenterDiv = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items:center;
+`
+
+export const TextType = styled.p`
+    font-weight: 300;
+    font-size: 15px;
+    font-family: "Roboto", sans-serif;
+`
+
+export const Button = styled.a`
+border: 1px solid blue;
+border-radius:5px;
+width:100px;
+text-align:center;
+background:blue;
+color:white;
+cursor: pointer;
+
+`
