ecc33f85a80497aefb8d8ec320619a556a37da47
Maria Estela <maria.estela@ifood.com.br>
2022-05-03 23:12:45 -0300

    logic to connect

--- src/Views/Conections/Conections.jsx
+++ src/Views/Conections/Conections.jsx
@@ -1,105 +1,62 @@
-import { Button } from "@mui/material";
 import { useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
+
+import { ConectionsContainer } from "./Conections.style";
 import { useAuth } from "../../firebase";
-import userQueryParams from "../Perfil/userQueryParams";
-import { ConectionsContainer, RequestPending } from "./Conections.style";
+import { useHistory } from "react-router-dom";
 
 function ConectionsPending () {
-  let query = userQueryParams();
-  const email = query.get("email");
-  const { getUserProfile, getUserDocs, getUserEvaluations } = useAuth();
-  const [currentUser, setCurrentUser] = useState({});
-  const [loading, setLoading] = useState(true);
-  const [shouldRedirect, setShouldRedirect] = useState(false);
-  const [currentUserLogged, setCurrentUserLogged] = useState({});
+  const email = localStorage.getItem("uid");
+  const {  getAllConnections, updateConnections } = useAuth();
+  const [connection, setConnection] = useState([])
+  const strObj = JSON.parse(localStorage.getItem("userInfo"))
+  const isGuard = strObj.type
+  
+  const getAllConnectionsStatus = () => {
+    getAllConnections(email).then(res =>{
+      res.get().then(doc =>{
+        let docs = []
+        doc.forEach( value =>{
+          docs = [...docs, { id: value.id, ...value.data() }];
+        })
+        setConnection( [...docs]);
+      })
+      
+    })
+  }
 
-  let listCurrentConections = currentUserLogged.currentConections
-  let listPendingConections = currentUserLogged.pendingConections
-  let listFinishedConections = currentUserLogged.finishedConections
-
-  let isCurrent = false
-  let isFeedback = false
-  let isPending = false
 
   const linkStyle = {
     textDecoration: "underline",
     color: 'blue'
   };
   let history = useHistory();
+  useEffect( () => {
+    getAllConnectionsStatus()
+  }, []);
 
-  useEffect(() => {
-    getUserInfo();
-    getDocs();
-    getEvaluations();
-  }, [query]);
 
-  const getUserInfo = () => {
-    getUserProfile(email)
-      .then((user) => {
-        setLoading(true);
-        setCurrentUser(user.data());
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.log("Error getting documents: ", error);
-        setLoading(false);
-      });
-
-      setCurrentUserLogged(JSON.parse(localStorage.getItem("userInfo")));
-  };
-
-  const getDocs = () => {
-    getUserDocs(email).then((response) => {
-      setLoading(true);
-      response.get().then((querySnapshot) => {
-        let docs = [];
-        querySnapshot.forEach((doc) => {
-          docs = [...docs, { id: doc.id, ...doc.data() }];
-        });
-
-        console.log(docs);
-        setCurrentUser((prev) => ({ ...prev, docs }));
-        setLoading(false);
-      });
-    });
-  };
-
-  const getEvaluations = () => {
-    getUserEvaluations(email).then((response) => {
-      setLoading(true);
-      response.get().then((querySnapshot) => {
-        let evaluations = [];
-        querySnapshot.forEach((doc) => {
-          evaluations = [...evaluations, { id: doc.id, ...doc.data() }];
-        });
-
-        setCurrentUser((prev) => ({ ...prev, evaluations }));
-        setLoading(false);
-      });
-    });
-  };
   return (
-    
     <ConectionsContainer>
+    { isGuard == 'guard' ? <>
       {
-        currentUserLogged.pendingConections > 0 ?
-        (
-          currentUserLogged.pendingConections.map((element) => {
+        connection.map(dado => (
+          <>
+          <div>
 
-          <RequestPending>
-            {element.name}
-            <div>
-              <Button variant="contained" color="success">Aceitar</Button>
-              <Button variant="contained" color="error">Cancelar</Button>
-            </div>
-          </RequestPending>
-          })
-        ) : (
-          
-          <h1>Não há conexões pendentes</h1>
-        )
+          <p>{dado.nome}</p>
+          <p>{dado.email_guard}</p>
+          <p>{dado.status_connection}</p>
+          { dado.status_connection == '0' ? <button onClick={()=> {
+            console.log("logica do update aqui ")
+            updateConnections(email,dado.email_guard,'1')
+          }}>connectar</button>: <h2>conexao aceita</h2>}
+          </div>
+        
+          </>
+        ))
       }
+
+   </>: <>Pagina não disponivel para lojistas  </> } 
     </ConectionsContainer>
 
     
