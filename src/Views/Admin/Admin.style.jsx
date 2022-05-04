c8447bfaeba831b1244950779a1b8a15e54e2cfa
Giovaniavs <gavs2@cin.ufpe.br>
2022-05-03 21:51:45 -0300

    fix bugs on admin style list

--- src/Views/Admin/Admin.style.jsx
+++ src/Views/Admin/Admin.style.jsx
@@ -9,12 +9,10 @@ export const ListSecurityContainer = styled.div`
   justify-content: center;
   align-items: flex-start;
   flex-direction: column;
-
   h1 {
-    margin: 15px;
-
+    margin: 15px 5px;
+    font-size: 1.5rem;
   }
-
   button.btn-plus {
     background: none;
     border: 1px solid #0A639E;
@@ -25,7 +23,6 @@ export const ListSecurityContainer = styled.div`
     height: 40px;
     border-radius: 10px;
   }
-
 `;
 
 
@@ -47,7 +44,6 @@ export const ListEmpty = styled.div`
     flex-direction: column;
     width: 100%;
     height: 50%;
-
     h1 {
       font-size: 2rem;
     }
@@ -57,7 +53,6 @@ export const ListEmpty = styled.div`
       text-align: center;
     }
   }
-
   button {
     width: 50%;
     min-width: 200px;
@@ -74,24 +69,30 @@ export const ListNotEmpty = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
+  justify-content: flex-start;
   align-items: center;
   flex-direction: column;
+  /* background-color: #DDDDDD; */
+  /* padding: 5%; */
   border-radius: 10px;
-  overflow: auto;
+  
   `;
 
 export const ItemList = styled.div`
-  width: 100%;
+  width: 95%;
   /* height: 00px; */
   /* border: 1px solid red; */
-  margin: 0px 0px 15px 0px; 
+  margin: 10px 0px 10px 0px; 
   display: flex;
   justify-content: flex-start;
   align-items: center;
+  box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.75);
   /* background-color: #DDDDDD; */
+  /* overflow: hidden; */
+  border-radius: 10px;
   
   img {
-    border-radius: 10px;
+    border-radius: 10px 0px 0px 10px;
   
     width: 30%;
     height: 100px;
@@ -99,23 +100,24 @@ export const ItemList = styled.div`
   }
   
   div.begin {
+    padding: 3%;
     background-color: #FFF;
     width: 50%;
     height: 100px;
     display: flex;
-    justify-content: space-around;
-    align-items: center;
+    justify-content: flex-start;
+    align-items: flex-start;
     flex-direction: column;
-
+    /* border-radius: 10px; */
     h6 {
       font-size: 1.2rem;
+      margin: 2%;
     }
-
     div.score {
       display: flex;
       justify-content: center;
       align-items: center;
-
+      border-radius: 10px;
       p {
         font-size: 1.1rem;
         margin: 0 5px;
@@ -131,7 +133,6 @@ export const ItemList = styled.div`
     padding-right: 15px;
     height: 100px;
     width: 20%;
-
+    border-radius: 0px 10px 10px 0px;
   }
-
 `;
