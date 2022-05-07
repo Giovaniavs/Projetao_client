779141774a1cd027251d143af5b48ab00e25cfce
Maria Estela <maria.estela@ifood.com.br>
2022-05-07 14:03:18 -0300

    style screen

--- src/Views/Conections/Conections.style.jsx
+++ src/Views/Conections/Conections.style.jsx
@@ -1,15 +1,12 @@
 import styled from "styled-components";
 
 export const ConectionsContainer = styled.div`
-  width: 100%;
-  height: 100%;
+  
   border-radius: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
-  border: 1px solid red;
-
+  flex-direction: column
 `;
 
 export const RequestPending = styled.div`
