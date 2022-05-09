e27ca6467f4a50c853cd02a200266812f2ad18fe
Daniel Carvalho <dnc331@gmail.com>
2022-05-08 21:29:52 -0300

    change perfil request certifications

--- src/Views/PerfilRequestRegister/PerfilRequestRegister.jsx
+++ src/Views/PerfilRequestRegister/PerfilRequestRegister.jsx
@@ -1,8 +1,8 @@
 import { Description, DocImg, DocLink, Docs, Wrapper } from "./styles";
 import React, { useEffect, useState } from "react";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 import ProfilePicResume from "../../components/ProfilePicResume";
 import ReactLoading from "react-loading";
 import Topic from "../../components/Topic";
@@ -21,7 +21,7 @@ export default function PerfilRequestRegister() {
 
   const linkStyle = {
     textDecoration: "underline",
-    color: 'blue'
+    color: "blue",
   };
   let history = useHistory();
 
@@ -42,7 +42,6 @@ export default function PerfilRequestRegister() {
         console.log("Error getting documents: ", error);
         setLoading(false);
       });
-
   };
 
   const getDocs = () => {
@@ -83,14 +82,23 @@ export default function PerfilRequestRegister() {
 
   const activateAccount = async () => {
     setLoadingActivation(true);
-    
-    await emailjs.send('service_z54odph', 'template_e6718ro', templateParams, '4CqV65zx_cKzGrtHB')
-    .then(async () => {
-      await setVerification(currentUser.email, true);
-      window.location.replace("/home")
-    }, (error) => {
-        console.log(error.text);
-    });
+
+    await emailjs
+      .send(
+        "service_z54odph",
+        "template_e6718ro",
+        templateParams,
+        "4CqV65zx_cKzGrtHB"
+      )
+      .then(
+        async () => {
+          await setVerification(currentUser.email, true);
+          window.location.replace("/home");
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
   };
 
   if (loading)
@@ -115,10 +123,16 @@ export default function PerfilRequestRegister() {
       <Topic name="Certificações">
         <Docs>
           {currentUser.docs &&
-            currentUser.docs.map(({ url, name }, index) => {
+            currentUser.docs[0].images.map((image, index) => {
+              // console.log({currentUser});
               return (
-                <DocLink href={url} target="_blank" key={index} title={name}>
-                  <DocImg src={url} alt={name} />
+                <DocLink
+                  href={image.url}
+                  target="_blank"
+                  key={index}
+                  title={image.name}
+                >
+                  <DocImg src={image.url} alt={image.name} />
                 </DocLink>
               );
             })}
@@ -127,24 +141,39 @@ export default function PerfilRequestRegister() {
 
       <Topic name="Contato">
         <Description>Entre em contato via whatsapp:</Description>
-          <a style={linkStyle} href={`https://wa.me/+55${currentUser.contact}?text=Olá ${currentUser.name}, gostaria de entrar em contato para contratação de seu serviço como segurança! Ví o seu perfil através do App MeSafe e tenho interesse em seu perfil!`}>{currentUser.contact}</a>
+        <a
+          style={linkStyle}
+          href={`https://wa.me/+55${currentUser.contact}?text=Olá ${currentUser.name}, gostaria de entrar em contato para contratação de seu serviço como segurança! Ví o seu perfil através do App MeSafe e tenho interesse em seu perfil!`}
+        >
+          {currentUser.contact}
+        </a>
       </Topic>
-      
+
       {loadingActivation ? (
         <ReactLoading
-        className="loading-login-screen-style"
-        type="bars"
-        color="#09629E"
-        height={"20%"}
-        width={"20%"}
-      />
+          className="loading-login-screen-style"
+          type="bars"
+          color="#09629E"
+          height={"20%"}
+          width={"20%"}
+        />
       ) : (
-        <div style={{ margin: '50px 0 0 0' }}>
-        <Button variant="contained" style={{ margin: '0 20px 0 0' }} onClick={activateAccount}>Aprovar</Button>
-        <Button variant="outlined" onClick={() => history.push(`/recusar?email=${currentUser.email}`)}>Não aprovar</Button>
-      </div>
+        <div style={{ margin: "50px 0 0 0" }}>
+          <Button
+            variant="contained"
+            style={{ margin: "0 20px 0 0" }}
+            onClick={activateAccount}
+          >
+            Aprovar
+          </Button>
+          <Button
+            variant="outlined"
+            onClick={() => history.push(`/recusar?email=${currentUser.email}`)}
+          >
+            Não aprovar
+          </Button>
+        </div>
       )}
-      
     </Wrapper>
   );
 }
